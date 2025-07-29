612b8642f9aade6f6865dbe765114a0f6e5aa6a9
Vlad Pr <149959639+vladprrs@users.noreply.github.com>
2025-07-30 04:56:07 +0700

    Add ContentManager and content components

---
+++ src/components/Content/DashboardContent.ts
@@ -0,0 +1,6 @@
+export class DashboardContent {
+  public element: HTMLElement;
+  constructor(container: HTMLElement) {
+    this.element = container;
+  }
+}
---
+++ src/components/Content/ResultsContent.ts
@@ -0,0 +1,6 @@
+export class ResultsContent {
+  public element: HTMLElement;
+  constructor(container: HTMLElement) {
+    this.element = container;
+  }
+}
